dcd46a005d09c02fc16ab8a129a51770f8d6fa84
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-13 16:47:43 +0530

    Create Q1.c

---
+++ Q1.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    int k;
+    printf("Enter number\t");
+    scanf("%d",&k);
+    int j = k/10;
+    printf("unit digit is %d",j);
+  
+    
+    return 0;
+}
