68debea2bdd29b8d7fd39b313e5693e992e31950
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-14 14:30:18 +0530

    Create Q6.c

---
+++ Q6.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main() {
+    char input;
+    printf("Enter character\t");
+    scanf("%c",&input);
+    int asciivalue  = input;
+    printf("Ascii value %d",asciivalue);
+    
+    
+    return 0;
+}
