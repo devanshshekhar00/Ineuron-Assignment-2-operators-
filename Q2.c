72240bd455a9d726705bbd1f53afdc97a1455fb0
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-14 11:13:18 +0530

    Create Q2.c

---
+++ Q2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int k;
+    printf("Enter the numer\t ");
+    scanf("%d",&k);
+    int l = k/10;
+    printf("Removing the unit digit %d",l);
+    
+    
+    return 0;
+}
