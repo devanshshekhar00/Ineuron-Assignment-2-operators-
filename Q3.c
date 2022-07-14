3c6e281459ca06d72f7f57786adf6cab6a6a23eb
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-14 11:18:23 +0530

    Create Q3.c

---
+++ Q3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    printf("Enter 1st number\t ");
+    scanf("%d",&a);
+    printf("Enter the 2nd number\t");
+    scanf("%d",&b);
+    int k = a;
+    a = b;
+    b = k;
+    printf("The numbers have been swapped , num1 is now %d,num2 is now %d ",a,b);
+    
+    
+    return 0;
+}
