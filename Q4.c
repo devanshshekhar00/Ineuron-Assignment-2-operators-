fb492693f4140f0ab3eb312df5b5adbb0e42b440
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-07-14 13:19:29 +0530

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -1,16 +1,12 @@
-#include <stdio.h>
-
-int main() {
-    int a,b;
-    printf("Enter 1st number\t ");
-    scanf("%d",&a);
-    printf("Enter the 2nd number\t");
-    scanf("%d",&b);
-    int k = a;
-    a = b;
-    b = k;
-    printf("The numbers have been swapped , num1 is now %d,num2 is now %d ",a,b);
-    
-    
-    return 0;
+#include <stdio.h> 
+int main()
+{ 
+ int a,b; printf("Enter 1st number\t "); scanf("%d",&a); 
+ printf("Enter the 2nd number\t"); 
+ scanf("%d",&b); 
+ a = a+b; 
+ b = a-b; 
+ a = a-b; 
+ printf("The numbers have been swapped , num1 is now %d,num2 is now %d ",a,b);
+ return 0; 
 }
